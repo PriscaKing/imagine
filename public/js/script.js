e5894afc7fe345f9212c8ea4da5120c4b2f452ad
Priscilla King <priscyking3@gmail.com>
2024-06-07 16:18:17 +0000

    Math in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -72,75 +72,75 @@ const  bottle1 = {
   }
   
 
-//   // Arrays
-// const  bottle1 = {
-//   size: 'large',
-//   color:'yellow',
-// }
+  // Arrays
+const  bottle1 = {
+  size: 'large',
+  color:'yellow',
+}
 
 
 
-// const bottle2 = {
-//   size:'small',
-//   color: 'blue',
-// }
+const bottle2 = {
+  size:'small',
+  color: 'blue',
+}
 
 
-// const bottles = [bottle1, bottle2,]
-// bottles.push(bottle1);
-// bottles.push(bottle2);
-// bottles;
-// bottles[0].size;
+const bottles = [bottle1, bottle2,]
+bottles.push(bottle1);
+bottles.push(bottle2);
+bottles;
+bottles[0].size;
 
 
-// const date = new Date ();
-// date.getDay();
+const date = new Date ();
+date.getDay();
 
-// //if/Else
-// const age = 18;
-// if (age >= 18) {
-//   'You are true';
-// } else{
-//   'You are false';
-// }
+//if/Else
+const age = 18;
+if (age >= 18) {
+  'You are true';
+} else{
+  'You are false';
+}
 
-// //For/Loop (position of loop exp 1; position 2 where it ends;position 3 the kind if steps they will take. i++ increment operator/ i)
-// for(let i = 0; i <= 5; i+=2) {
-//   console.log('We did it!',i);
-// }
+//For/Loop (position of loop exp 1; position 2 where it ends;position 3 the kind if steps they will take. i++ increment operator/ i)
+for(let i = 0; i <= 5; i+=2) {
+  console.log('We did it!',i);
+}
 
-// //function
-// //defining a function
+//function
+//defining a function
 
-// function login(username, password){
-//   //validate the username and password
-//   if (username =='annice' && password == '1234'){
-//     return 'user is logged in';
-//   } else{
-//     return 'invalid username or password';
-//   }
-// }
+function login(username, password){
+  //validate the username and password
+  if (username =='annice' && password == '1234'){
+    return 'user is logged in';
+  } else{
+    return 'invalid username or password';
+  }
+}
 
-// //invoking a function
-// login('annice');
+//invoking a function
+login('annice');
 
-// //Baic Arithmetic operations
-// 11+12;
-// 1+0.5;
-// 43-12;
-// 45/7;
-// 5*3;
-// 14%3;
+//Baic Arithmetic operations
+11+12;
+1+0.5;
+43-12;
+45/7;
+5*3;
+14%3;
 
-// Math.floor(45/25);
-// 45%24;
-// Math.random()*1000
-// Math.max(34,32,37);
-// 2*(3+4)-5/2;
+Math.floor(45/25);
+45%24;
+Math.random()*1000
+Math.max(34,32,37);
+2*(3+4)-5/2;
 
 
-// //the use of Num()
-// Number('2')+2
+//the use of Num()
+Number('2')+2
 
 
 // //strings in JavaScript
