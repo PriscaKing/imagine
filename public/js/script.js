9d3e25dea8bc95ad485e6af9d513dee180124f57
Priscilla King <priscyking3@gmail.com>
2024-06-12 15:16:05 +0000

    Objects in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -71,7 +71,7 @@ const  bottle1 = {
     console.log('We did it!',i);
   }
   
-
+/////////
   // Arrays
 const  bottle1 = {
   size: 'large',
@@ -124,7 +124,7 @@ function login(username, password){
 //invoking a function
 login('annice');
 
-//Baic Arithmetic operations
+//Basic Arithmetic operations
 11+12;
 1+0.5;
 43-12;
@@ -172,3 +172,187 @@ parseFloat('3.243')
 let amount = 1800
 console.log('GHS${amount}')
 amount.toString()
+
+
+//Write a function that will add a participant to our Google Classroom
+
+// My Trial code
+// function add(newuser, password){
+//   if (newuser == 'Joe' && password == '5678')
+//     {
+//       return 'Signed up';
+//     } else {
+//       return 'return to log in';
+//     }
+// }
+
+// add('Joe');
+
+//He declared a push to add to the existing data but I didnt
+// if (email === undefined || email === null) is equal to (!email) ! means not
+
+
+const participants = [];
+function addParticipant(email) {
+  //Check if email was provided
+   if (!email){
+     return 'No email provided'
+   }
+  //check if email is valid
+  if (!email.includes('@')) {
+    return 'Invalid email provided';
+  }
+  //Add email to participant
+  participants.push(email); 
+  return 'Participant added';
+}
+addParticipant('kingpriscy@gmail.com');
+addParticipant('kingpriscy@gmail.com');
+addParticipant();
+participants;
+
+//Arrays in JavaScript
+const users =[
+  {
+  username: 'priscyk',
+  password:'1234',
+  email:'priscyking3@gmail.com'
+},
+{
+  username: 'Carlk',
+  password:'0000',
+  email:'carlk@gmail.com'
+}
+];
+users;
+
+
+//Write a function that will take a user with firstname,lastname and return fullname;
+
+//class code
+// function fullName(user){
+//   return user.firstname + ' ' + user.lastname;
+//   //return `${user.firstname} ${user.lastname}`;
+// }
+
+// const user = {
+//   firstname: 'Priscilla',
+//   lastname: 'King'
+// }
+// fullName(user);
+
+
+
+//class code
+// function fullName(user){
+//   return {
+//     ...user,
+//    fullName: `${user.firstname} ${user.lastname}`
+//   }
+//   }
+
+
+// const user = {
+//   firstname: 'Priscilla',
+//   lastname: 'King'
+// }
+// fullName(user);
+
+
+// //Array map
+// const users = [
+//   { firstname: 'Michael', lastname:'Hammond'},
+//   { firstname: 'Simon', lastname:'Kissi'},
+//   { firstname: 'Segoe', lastname:'Sherif'},
+//   { firstname: 'Nat', lastname:'Kumi'},
+//   { firstname: 'Seth', lastname:'Ham'},
+//   { firstname: 'Naana', lastname:'Mond'},
+// ]
+// users.map(fullName)
+
+
+
+
+
+
+//Square of numbers
+function square(number){
+  return number ** 2;
+}
+square(6);
+
+const numbers = [9, 8, 7, 6];
+numbers.map(square);
+
+
+//Array filter
+function isEven(number) {
+  return number % 2 === 0;
+  }
+isEven(6);
+numbers.filter(isEven);
+
+
+function isOdd(number) {
+  return number % 3 === 0;
+}
+isOdd(9)
+numbers.filter(isOdd);
+
+
+
+
+
+
+
+//Write a function that will allow a user to reset their password
+const user ={
+  email:'mickeymond@yahoo.com',
+  password: '1234',
+}
+
+//p-try
+// function reset(password){ 
+//   if (!password.includes('1234')){
+//     return 'reset password';
+//   } else {
+//     return 'valid'
+//   }
+// }
+//  reset('');
+
+
+//class attempt-1
+// function resetPassword(email, newPassword){
+//   //Update password with new one
+//   user.password = newPassword;
+//     // return 'Password reset successful'
+//   }
+// resetPassword();
+// user;
+// // newPassword;
+
+
+
+function resetPassword(email, newPassword){
+  ///check if email and new password was provided
+  if (!email || !newPassword){
+    return "Email or prassword not provided"; 
+  }
+  
+  //Check if provided email is correct
+  if(email === user.email){
+      //Update password with new one
+  user.password = newPassword;
+    return "Password reset successful";
+  }
+  if (email !== user.email){
+    return "Invalid email";
+  }
+}
+
+user;
+resetPassword("mickeymond@outlook.com");
+user;
+
+
